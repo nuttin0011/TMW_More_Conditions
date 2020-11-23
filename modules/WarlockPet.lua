38d624a76fe80364e0d49b2fd65049c3e2bbd0e7
nuttin0011 <nuttin0011@hotmail.com>
2020-11-24 00:56:10 +0700

    Update WarlockPet.lua

--- modules/WarlockPet.lua
+++ modules/WarlockPet.lua
@@ -10,3 +10,31 @@ local _CombatLogGetCurrentEventInfo = CombatLogGetCurrentEventInfo
 
 local CNDT = TMW.CNDT
 local Env = CNDT.Env
+
+print('lockpet')
+
+local function printtable(a)
+local k,v
+    for k,v in pairs(a) do
+        print(k,v)
+    end
+
+end
+
+local ConditionCategory = CNDT:GetCategory("ATTRIBUTES_TMWMC", 12, "More Conditions", true, false)
+
+ConditionCategory:RegisterCondition(8.6,  "TMWMCWARLOCKPET", {
+    text = "Warlock Pet Active",
+    tooltip = "Warlock Pet Active",
+    unit="Pet",
+    bitFlagTitle = "select Pet",
+    bitFlags={"Felguard", "Succubus", "Felhunter", "Voidwalker", "Imp"},
+    icon = "Interface\\Icons\\ability_druid_bash",
+    tcoords = CNDT.COMMON.standardtcoords,
+
+    funcstr = function(c)
+        --printtable(c)
+        return [[true]]
+    end
+})
+

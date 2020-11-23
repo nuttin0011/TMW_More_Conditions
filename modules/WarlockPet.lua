c47e9b7946ead930969c3efacb960814133e6dc5
nuttin0011 <nuttin0011@hotmail.com>
2020-11-23 22:58:38 +0700

    Create WarlockPet.lua

---
+++ modules/WarlockPet.lua
@@ -0,0 +1,12 @@
+local TMW = TMW
+local TMW_MC = TMW_More_Conditions
+
+--caching
+
+local _GetTime = GetTime
+local _UnitPower = UnitPower
+local _UnitCastingInfo = UnitCastingInfo
+local _CombatLogGetCurrentEventInfo = CombatLogGetCurrentEventInfo
+
+local CNDT = TMW.CNDT
+local Env = CNDT.Env

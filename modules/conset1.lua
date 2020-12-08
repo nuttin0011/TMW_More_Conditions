05117d0a2ae1caabc3f3cac24a610cf6d5431433
nuttin0011 <nuttin0011@hotmail.com>
2020-12-08 17:56:39 +0700

    Update conset1.lua

--- modules/conset1.lua
+++ modules/conset1.lua
@@ -132,14 +132,19 @@ ConditionCategory:RegisterCondition(8.5,  "TMWMCPREDICTSS", {
 
 --****************************percentCastBar*******************************
 
-Env.percentCastBar = function(SpellN)
-    return TMW_MC:PercentCastBar(SpellN)
+Env.percentCastBar = function(SpellN,nUnit)
+    return TMW_MC:PercentCastBar(SpellN,nUnit)
 end
 
 local old_val_PercentCastBar = 0
 local old_timer_PercentCastBar = 0
 
-function TMW_MC:PercentCastBar(SpellN)
+function TMW_MC:PercentCastBar(SpellN,nUnit)
+
+	nUnit = nUnit or "target"
+	SpellN = SpellN or ""
+	if SpellN == ";" then SpellN = "" end
+	
 	local currentTimeMS = _GetTime()
 	if currentTimeMS==old_timer_PercentCastBar then
 		return old_val_PercentCastBar
@@ -147,10 +152,10 @@ function TMW_MC:PercentCastBar(SpellN)
 
 	old_timer_PercentCastBar = currentTimeMS
 
-	local spellName,_,_, startTimeMS, endTimeMS = _UnitCastingInfo("player")
+	local spellName,_,_, startTimeMS, endTimeMS = _UnitCastingInfo(nUnit)
 
 	if spellName then
-		if (spellName==SpellN) or (SpellN==nil) or (SpellN=="") then
+		if (spellName==SpellN) or (SpellN=="") then
 			local currentTimeMS = currentTimeMS*1000
 			local PercentCast = (currentTimeMS-startTimeMS)/(endTimeMS-startTimeMS)
 			old_val_PercentCastBar = PercentCast
@@ -167,13 +172,13 @@ function TMW_MC:PercentCastBar(SpellN)
 end
 
 ConditionCategory:RegisterCondition(8.6,  "TMWMCPERCENTCAST", {
-    text = "% cast bar this spell",
-    tooltip = "% cast bar this spell",
+    text = "% cast bar unit's spell",
+    tooltip = "% cast bar unit's spell",
 	step = 5,
 	percent = true,
     min = 0,
 	max = 100,
-    unit="player",
+    unit=nil,
 
     icon = "Interface\\Icons\\ability_druid_bash",
     tcoords = CNDT.COMMON.standardtcoords,
@@ -189,7 +194,7 @@ ConditionCategory:RegisterCondition(8.6,  "TMWMCPERCENTCAST", {
     end,
 
 	funcstr = function(c, parent)
-        return [[(percentCastBar() c.Operator c.Level)]]
+        return [[(percentCastBar(nil,c.Unit) c.Operator c.Level)]]
     end
 })
 
@@ -211,19 +216,11 @@ function TMW_MC:HowManyMobHasMyDot()
 	end
 	old_timer_HowManyMobHasMyDot=currentTime
     n = 0
-
         for ii = 1,30 do
-
         nn = 'nameplate'..ii
-
         if UnitExists(nn) and UnitDebuff(nn, 1,"PLAYER") then
-
-            
-            
             n = n+1
-
         end
-
     end
 
 old_val_HowManyMobHasMyDot=n

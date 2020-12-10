409e9ec437115b3f3da210883d1ac9e5ccceb936
nuttin0011 <nuttin0011@hotmail.com>
2020-12-11 02:21:24 +0700

    Update conset2.lua

--- modules/conset2.lua
+++ modules/conset2.lua
@@ -23,6 +23,18 @@ local function printtable(a)
 
 local ConditionCategory = CNDT:GetCategory("ATTRIBUTES_TMWMC", 12, "More Conditions", true, false)
 
+--**************************Lowest Duration Debuff*********************
 
+function TMW_MC:LowestDurationDebuff(nDebuff,nSetDebuff)
+	-- nDebuff is Debuff to check e.g. "Agony"
+	-- nSetDebuff is Set of Defuff to check e.g. "Agony; Corruption; Unstable Affliction"
+	-- return true if nDebuff has lowest duration
+	
+	if not nDebuff then return false end
+	nSetDebuff = nSetDebuff or ""
+	if nSetDebuff = ";" then nSetDebuff="" end
+
+	return false
+end
 
 

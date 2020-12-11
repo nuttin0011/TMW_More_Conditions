91497d4a1e6375ea70f336a62c0d706122f62d9d
nuttin0011 <nuttin0011@hotmail.com>
2020-12-11 14:51:54 +0700

    Create testtest.lua

---
+++ modules/testtest.lua
@@ -0,0 +1,39 @@
+-- testes is 2 testis
+local TMW = TMW
+local TMW_MC = TMW_More_Conditions
+local CNDT = TMW.CNDT
+local Env = CNDT.Env
+
+
+function Env.TMWMCTest(a,b,c,d)
+	print('====')
+	print(a)
+	print(b)
+	print(c)
+	print(d)	
+	return true
+end
+
+
+local ConditionCategory = CNDT:GetCategory("ATTRIBUTES_TMWMC_Test", 14, "Test Condition", true, false)
+
+ConditionCategory:RegisterCondition(6,  "TMWMCTEST1111", {
+    text = "T111",
+	tooltip = "T222",
+	unit1=nil,
+	unit2=nil,
+	name=function(editbox) 
+		editbox:SetTexts("test1111","e.g. target")
+	end,
+	noslide = true,
+	nooperator = true,
+    icon = "Interface\\Icons\\spell_nature_rejuvenation",
+    tcoords = CNDT.COMMON.standardtcoords,
+
+	funcstr = function(c, parent)
+		
+		--return [[TMWMCTest(c.Name,c.NameFirst,c.NameRaw,c.NameString)]]
+		return [[true]]
+    end,	
+
+})

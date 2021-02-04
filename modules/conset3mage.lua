906b01df1f99084f39ba448d6cad4f519ddaa921
nuttin0011 <nuttin0011@hotmail.com>
2021-02-04 11:47:19 +0700

    Create conset3mage.lua

---
+++ modules/conset3mage.lua
@@ -0,0 +1,29 @@
+local TMW = TMW
+local TMW_MC = TMW_More_Conditions
+
+-- for mage
+--[[
+local f = CreateFrame("Frame")
+f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+f:SetScript("OnEvent", function(self, event)
+	self:COMBAT_LOG_EVENT_UNFILTERED(CombatLogGetCurrentEventInfo())
+end)
+
+function f:COMBAT_LOG_EVENT_UNFILTERED(...)
+	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
+	local spellId, spellName, spellSchool
+	local amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand
+
+	if subevent == "SWING_DAMAGE" then
+		amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
+	elseif subevent == "SPELL_DAMAGE" then
+		spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
+	end
+
+	if critical and sourceGUID == playerGUID then
+		-- get the link of the spell or the MELEE globalstring
+		local action = spellId and GetSpellLink(spellId) or MELEE
+		print(MSG_CRITICAL_HIT:format(action, destName, amount))
+	end
+end
+--]]

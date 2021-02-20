38662e5165df126f4ddf9cb6924f0416c10176af
nuttin0011 <nuttin0011@hotmail.com>
2021-02-20 18:45:04 +0700

    Create conset5ERO.lua

---
+++ modules/conset5ERO.lua
@@ -0,0 +1,85 @@
+-- this function Change Normal IROcode --> miniIROCode
+-- it's ll compress 3 Icon --> 1 Icon
+-- ctrl		= 001 00000 = 32	= 0x20
+-- alt		= 010 00000 = 64	= 0x40
+-- shift	= 100 00000 = 128	= 0x80
+-- e.g.
+-- c1 = "ff002100" (F1)			--> 0x01
+-- c2 = "ff003000" ({Numpad0})	--> 0x0d
+-- c3 = "ff042604" (Shift-F6)	--> 0x06 + 0x80 (shift code) --> 0x86
+-- c1+c2+c3 = miniIROCode = "ff860d01"
+-- NOTE. need to swap byte c1 and c3 cause of EroDPS PixelGetColor function
+-- TMW_MC:enMiniIROcode("ff002100","ff003000","ff042604")="ff860d01"
+
+local IROcolorCode ={
+	["00"]=0x00,
+	["21"]=0x01, --{F1}
+	["22"]=0x02, --{F2}
+	["23"]=0x03, --{F3}
+	["24"]=0x04, --{F4}
+	["25"]=0x05, --{F5}
+	["26"]=0x06, --{F6}
+	["27"]=0x07, --{F7}
+	["28"]=0x08, --{F8}
+	["29"]=0x09, --{F9}
+	["2a"]=0x0a, --{F10}
+	["2b"]=0x0b, --{F11}
+	["2c"]=0x0c, --{F12}
+	["30"]=0x0d, --{Numpad0}
+	["31"]=0x0e, --{Numpad1}
+	["32"]=0x0f, --{Numpad2}
+	["33"]=0x10, --{Numpad3}
+	["34"]=0x11, --{Numpad4}
+	["35"]=0x12, --{Numpad5}
+	["36"]=0x13, --{Numpad6}
+	["37"]=0x14, --{Numpad7}
+	["38"]=0x15, --{Numpad8}
+	["39"]=0x16, --{Numpad9}
+	["3a"]=0x17, --{NumpadDot}	
+	["3b"]=0x18, --{NumpadAdd}
+	["3c"]=0x19, --{NumpadSub}
+	["3d"]=0x1a, --{NumpadMult}
+	["3e"]=0x1b, --{NumpadDiv}
+	["55"]=0x1c, --MoveJump ; 0x005500 : MoveJump
+}
+
+-- 1 = ctrl , 2 = alt , 4 = shift
+-- 1 = 001 , 2 = 010 , 4 = 100
+local modStr ={
+	["00"]=0,
+	["01"]=bit.lshift(0x01,5),
+	["02"]=bit.lshift(0x02,5),
+	["03"]=bit.lshift(0x03,5),
+	["04"]=bit.lshift(0x04,5),
+	["05"]=bit.lshift(0x05,5),
+	["06"]=bit.lshift(0x06,5),
+	["07"]=bit.lshift(0x07,5),
+}
+
+function TMW_MC:enSubMiniIROCode(IROcode)
+	--"ff042604" (Shift-F6)	--> 0x06 + 0x20 (shift code) --> 0x26
+	local modstr = modStr[string.sub(IROcode,3,4)]
+	if not modstr then return -1 end
+	local miniIROCode = IROcolorCode[string.sub(IROcode,5,6)]
+	if not miniIROCode then return -1 end
+	return string.format("%02x",miniIROCode+modstr)
+end
+
+function TMW_MC:getMetaIconColor(icon)
+	local a=icon.__currentIcon
+	local b=a and a.__currentIcon or nil
+	while b do
+		a=b
+		b=a.__currentIcon
+	end
+	local c=a and a.States[1].Color or "ff000000"
+	return c
+end
+
+function TMW_MC:enMiniIROcode(IROcode1,IROcode2,IROcode3)
+	local miniIROCode1 = TMW_MC:enSubMiniIROCode(IROcode1)
+	local miniIROCode2 = TMW_MC:enSubMiniIROCode(IROcode2)
+	local miniIROCode3 = TMW_MC:enSubMiniIROCode(IROcode3)
+	return "ff"..miniIROCode3..miniIROCode2..miniIROCode1
+end
+

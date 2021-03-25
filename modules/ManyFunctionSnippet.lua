ceb2ad4daa18917dcec06ce2908afeef10406d65
nuttin0011 <nuttin0011@hotmail.com>
2021-03-25 19:02:40 +0700

    Update ManyFunctionSnippet.lua

--- modules/ManyFunctionSnippet.lua
+++ modules/ManyFunctionSnippet.lua
@@ -242,28 +242,24 @@ function PercentCastbar2(PercentCast, MustInterruptAble,unit, MinTMS,MaxTMS)
     MaxTMS = MaxTMS or 2000
     MinTMS = MinTMS or 200
     unit = unit or "target"
-    local castingName, _, _, startTimeMS, endTimeMS, _, _, notInterruptible = UnitCastingInfo(unit)
+    local _, _, _, startTimeMS, endTimeMS, _, _, notInterruptible = UnitCastingInfo(unit)
     local wantInterrupt = false
     local totalcastTime
     local currentcastTime
     local percentcastTime
-    if (castingName ~= nil) and(not(notInterruptible and MustInterruptAble)) then
+    if (startTimeMS ~= nil) and(not(notInterruptible and MustInterruptAble)) then
         totalcastTime = endTimeMS-startTimeMS
         currentcastTime = (GetTime()*1000)-startTimeMS
-        if (totalcastTime-currentcastTime)>MaxTMS then
-            -- if cast time > MaxTMS ms dont interrupt
-            wantInterrupt = false
-        elseif (totalcastTime-currentcastTime)<MinTMS then
-            -- if cast time < MinTMS ms dont interrupt
-            wantInterrupt = false
-        else
+    -- if cast time > MaxTMS ms dont interrupt
+    -- if cast time < MinTMS ms dont interrupt
+        if ((totalcastTime-currentcastTime)>=MinTMS) and ((totalcastTime-currentcastTime)<=MaxTMS) then
             percentcastTime = currentcastTime/totalcastTime
             wantInterrupt = percentcastTime >= PercentCast
         end
         return wantInterrupt
     end
-    local channelName, _, _, CstartTimeMS, CendTimeMS,_, CnotInterruptible= UnitChannelInfo(unit)
-    if (channelName ~= nil) and (not (CnotInterruptible and MustInterruptAble)) then
+    local _, _, _, CstartTimeMS, CendTimeMS,_, CnotInterruptible= UnitChannelInfo(unit)
+    if (CstartTimeMS ~= nil) and (not (CnotInterruptible and MustInterruptAble)) then
         totalcastTime = CendTimeMS-CstartTimeMS
         currentcastTime = (GetTime()*1000)-CstartTimeMS
         if (currentcastTime>=MinTMS) and (currentcastTime<=(totalcastTime-MinTMS)) then
@@ -370,10 +366,6 @@ local IROClassGCDOneSec = {
     [103]=true, -- druid feral
 }
 
-local function round(number, decimals)
-    return (("%%.%df"):format(decimals)):format(number)
-end
-
 function GCDCDTime()
     --return GCD CD
     local OldVal=IROVar.ERO_Old_Val.Check("GCDCDTime","")
@@ -384,7 +376,7 @@ function GCDCDTime()
         if IROClassGCDOneSec[IROSpecID] then
             GCDCD = 1
         else
-            GCDCD = round(1.5*(100/(100+UnitSpellHaste("player"))),2)
+            GCDCD = 1.5*(100/(100+UnitSpellHaste("player")))
         end
     end
     IROVar.ERO_Old_Val.Update("GCDCDTime","",GCDCD)

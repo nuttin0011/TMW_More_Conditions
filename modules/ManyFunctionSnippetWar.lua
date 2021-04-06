39422f78152d70eadff80d8e336802ae2578fb4b
nuttin0011 <nuttin0011@hotmail.com>
2021-04-06 08:58:54 +0700

    Update ManyFunctionSnippetWar.lua

--- modules/ManyFunctionSnippetWar.lua
+++ modules/ManyFunctionSnippetWar.lua
@@ -1,24 +1,50 @@
 -- Many Function Version War 9.0.5/1
 -- this file save many function for paste to TMW Snippet LUA
 
---function IsUsableExecute(nUnit) ; return true/false
+--function IROVar.War.CanExx(U) ; return true/false
 
-IROVar.ItemNameToCheck2Yd = "item:37727"
+if not IROVar then IROVar={} end
+IROVar.War={}
+IROVar.War.It2Yd = "item:37727"
+IROVar.War.isMass = nil
+IROVar.War.isCondemn = nil
 
-function IsUsableExecute(nUnit)
-    nUnit=nUnit or "target"
-    local OldVal=IROVar.ERO_Old_Val.Check("IsUsableExecute",nUnit)
+
+
+
+function IROVar.War.CanExx(U)
+    U=U or "target"
+    if IROVar.War.isMass==nil then IROVar.War.SetupTalentCheck() end
+    local OldVal=IROVar.ERO_Old_Val.Check("War.CanExx",U)
     if OldVal then return OldVal end
     local uH ,uHM, uHP, output
-    if UnitCanAttack("player", nUnit) and IsItemInRange(IROVar.ItemNameToCheck2Yd, nUnit) then
-        uHM=UnitHealthMax(nUnit)
-        uH=UnitHealth(nUnit)
-        uHP=(uH/uHM)*100
-        output=(uHP>0) and ((uHP<20) or ((uHP<35) and IROVar.isMassacre) or ((uHP>80) and IROVar.isCondemn))
-        IROVar.ERO_Old_Val.Update("IsUsableExecute",nUnit,output)
+    if UnitCanAttack("player", U) and IsItemInRange(IROVar.War.It2Yd, U) then
+        uHP=(UnitHealth(U)/UnitHealthMax(U))*100
+        output=(uHP>0) and ((uHP<20) or ((uHP<35) and IROVar.War.isMass) or ((uHP>80) and IROVar.War.isCondemn))
+        IROVar.ERO_Old_Val.Update("War.CanExx",U,output)
         return output
     else
-        IROVar.ERO_Old_Val.Update("IsUsableExecute",nUnit,false)
+        IROVar.ERO_Old_Val.Update("War.CanExx",U,false)
         return false
     end
 end
+
+function IROVar.War.CheckTalent()
+    local _,TName,_,TSelected=GetTalentInfo(3,1,1)
+    IROVar.War.isMass = (TName=="Massacre") and TSelected
+    IROVar.War.isCondemn = GetSpellInfo("execute")=="Condemn"
+end
+
+function IROVar.War.SetupTalentCheck()
+    IROVar.War.FOnEvent=function()
+        C_Timer.After(1,IROVar.War.CheckTalent)
+    end
+    IROVar.War.CheckTalent()
+    IROVar.War.FEvent = CreateFrame("Frame")
+    IROVar.War.FEvent:RegisterEvent("PLAYER_TALENT_UPDATE")
+    IROVar.War.FEvent:SetScript("OnEvent", IROVar.War.FOnEvent)
+end
+
+
+
+

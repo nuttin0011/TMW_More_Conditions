d4badbe162179653ea52ddd563ae7183472aab54
nuttin0011 <nuttin0011@hotmail.com>
2021-04-06 12:38:41 +0700

    Update ManyFunctionSnippetLock.lua

--- modules/ManyFunctionSnippetLock.lua
+++ modules/ManyFunctionSnippetLock.lua
@@ -1,29 +1,30 @@
--- Many Function Version Lock 9.0.5/1
+-- Many Function Version Warlock 9.0.5/1
 -- this file save many function for paste to TMW Snippet LUA
 
---function IROVar.LockPet(PetType) return true/false
+--function IROVar.Warlock.Pet(PetType) return true/false
 ----PetType 1=Felg 2=Succ 4=Felh 8=Voidw 16=Imp can use 3 for check felg+succ
 
-if not IROVar then IROVar={} end
 
-function IROVar.LockPet(PetType)
-    if IROVar.LockPetActive then return bit.band(IROVar.LockPetActive,PetType)~=0 end
-    IROVar.LockPetEvent = CreateFrame("Frame")
-    IROVar.LockPetOnEvent=function()
-        IROVar.LockPetActive=0
+if not IROVar then IROVar={} end
+IROVar.Warlock={}
+IROVar.Warlock.PetActive=nil
+function IROVar.Warlock.Pet(PetType)
+    PetType=PetType or 0
+    if IROVar.Warlock.PetActive then return bit.band(IROVar.Warlock.PetActivee,PetType)~=0 end
+    IROVar.Warlock.SetupPetEvent()
+    return IROVar.Warlock.Pet(PetType)
+end
+IROVar.Warlock.PetTypeBit={["Axe Toss"]=1,["Seduction"]=2,["Spell Lock"]=4,["Shadow Bulwark"]=8,["Singe Magic"]=16}
+function IROVar.Warlock.SetupPetEvent()
+    IROVar.Warlock.PetEvent=CreateFrame("Frame")
+    IROVar.Warlock.PetOnEvent=function()
+        IROVar.Warlock.PetActive=0
         local spellName = GetSpellInfo("Command Demon")
         if UnitExists("pet") and (not UnitIsDead("pet")) then
-            if spellName == "Axe Toss" then IROVar.LockPetActive = 1
-            elseif spellName == "Seduction" then IROVar.LockPetActive = 2
-            elseif spellName == "Spell Lock" then IROVar.LockPetActive = 4
-            elseif spellName == "Shadow Bulwark" then IROVar.LockPetActive = 8
-            elseif spellName == "Singe Magic" then IROVar.LockPetActive = 16
-            end
+            IROVar.Warlock.PetActive=IROVar.Warlock.PetTypeBit[spellName] or 0
         end
     end
-    IROVar.LockPetEvent:RegisterEvent("UNIT_PET")
-    IROVar.LockPetEvent:SetScript("OnEvent", IROVar.LockPetOnEvent)
-    IROVar.LockPetOnEvent()
-    return IROVar.LockPet(PetType)
+    IROVar.Warlock.PetEvent:RegisterEvent("UNIT_PET")
+    IROVar.Warlock.PetEvent:SetScript("OnEvent", IROVar.Warlock.PetOnEvent)
+    IROVar.Warlock.PetOnEvent()
 end
-

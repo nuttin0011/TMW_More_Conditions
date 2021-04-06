06836476e52becd59588f896e0aab48b6d2f4ac3
nuttin0011 <nuttin0011@hotmail.com>
2021-04-06 13:47:31 +0700

    Update ManyFunctionSnippetWar.lua

--- modules/ManyFunctionSnippetWar.lua
+++ modules/ManyFunctionSnippetWar.lua
@@ -1,24 +1,24 @@
 -- Many Function Version War 9.0.5/1
 -- this file save many function for paste to TMW Snippet LUA
 
---function IROVar.War.CanExx(U) ; return true/false
+--function IROVar.War.CanExx(Unit or blank = "target") ; return true/false
+--function IROVar.War.IsEquipShield() ; return true/false
 
 if not IROVar then IROVar={} end
 IROVar.War={}
-IROVar.War.It2Yd = "item:37727"
-IROVar.War.isMass = nil
-IROVar.War.isCondemn = nil
-
-
+IROVar.War.it2Yd = "item:37727"
+IROVar.War.isMass = false
+IROVar.War.isCondemn = false
+IROVar.War.isEquipShield = false
 
 
 function IROVar.War.CanExx(U)
     U=U or "target"
-    if IROVar.War.isMass==nil then IROVar.War.SetupTalentCheck() end
+    if not IROVar.War.FOnEvent then IROVar.War.SetupEventCheck() end
     local OldVal=IROVar.ERO_Old_Val.Check("War.CanExx",U)
     if OldVal then return OldVal end
-    local uH ,uHM, uHP, output
-    if UnitCanAttack("player", U) and IsItemInRange(IROVar.War.It2Yd, U) then
+    local uHP, output
+    if UnitCanAttack("player", U) and IsItemInRange(IROVar.War.it2Yd, U) then
         uHP=(UnitHealth(U)/UnitHealthMax(U))*100
         output=(uHP>0) and ((uHP<20) or ((uHP<35) and IROVar.War.isMass) or ((uHP>80) and IROVar.War.isCondemn))
         IROVar.ERO_Old_Val.Update("War.CanExx",U,output)
@@ -35,16 +35,27 @@ function IROVar.War.CheckTalent()
     IROVar.War.isCondemn = GetSpellInfo("execute")=="Condemn"
 end
 
-function IROVar.War.SetupTalentCheck()
-    IROVar.War.FOnEvent=function()
-        C_Timer.After(1,IROVar.War.CheckTalent)
+function IROVar.War.SetupEventCheck()
+    IROVar.War.FOnEvent=function(_,event)
+        print("Event in Warrior On Event",event)
+        if event == "PLAYER_TALENT_UPDATE" then
+            C_Timer.After(2,IROVar.War.CheckTalent)
+        elseif event == "UNIT_INVENTORY_CHANGED" then
+            local ItemLink=GetInventoryItemLink("player", 17)--shield
+            IROVar.War.isEquipShield=(ItemLink~=nil) and (select(7,GetItemInfo(ItemLink))=="Shields") or false
+        end
     end
-    IROVar.War.CheckTalent()
+    C_Timer.After(2,IROVar.War.CheckTalent)
     IROVar.War.FEvent = CreateFrame("Frame")
     IROVar.War.FEvent:RegisterEvent("PLAYER_TALENT_UPDATE")
+    IROVar.War.FEvent:RegisterEvent("UNIT_INVENTORY_CHANGED")
     IROVar.War.FEvent:SetScript("OnEvent", IROVar.War.FOnEvent)
 end
 
+function IROVar.War.IsEquipShield()
+    if not IROVar.War.FOnEvent then IROVar.War.SetupEventCheck() end
+    return IROVar.War.isEquipShield
+end
 
 
 

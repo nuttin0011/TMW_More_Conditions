7734fb8adaf45c037807d0663d23ebc0b40de736
nuttin0011 <nuttin0011@hotmail.com>
2021-04-07 21:05:22 +0700

    Update temp_debug.lua

--- modules/temp_debug.lua
+++ modules/temp_debug.lua
@@ -1,27 +1,32 @@
--- Spec 1 macro setting...
+-- Key Setting...
 local Nm={}
-local nname
 
---Nm[1]=''
---Nm[2]=''
 
-local iS
-if IROInterruptTier then iS=IROInterruptTier[IROSpecID][2]else iS="" end
+Nm[1]=''
+Nm[2]=''
+
+
+
+
+--print((IROVar.InterruptSpell~=nil) and IROVar.InterruptSpell or "nil")
+--print((IROSpecID~=nil) and IROSpecID or "nil")
+local iS=IROVar.InterruptSpell or ""
 Nm[8]=(Nm[8]or'')..'\n/targetenemy [nomod:ctrl,mod:alt]\n/cleartarget [nomod]'
 Nm[9]='/focus [@mouseover,exists,harm,nodead,mod:ctrlalt]'..
 '\n/cast [mod:ctrl,nomod:alt,@focus]'..iS..';[mod:alt,nomod:ctrl]'..iS..
 '\n/use [nomod]13'..
 '\n/use [nomod]14'
-
-if InCombatLockdown() then
-    print("cannot Bind Key while Incombat")
-end
-
-if not(InCombatLockdown()) then
-    for i in pairs(Nm) do
-        nname='~!Num'..i
-        DeleteMacro(nname)
-        DeleteMacro(nname)
-        CreateMacro(nname,460699,Nm[i] ,true)
-    end
-end
+local function SetKey(IncombatStatus)
+    if InCombatLockdown() then
+        if IncombatStatus then print("cannot Bind Key while Incombat") end
+        C_Timer.After(1,SetKey)
+    else
+        if not IncombatStatus then print("Out Combat Bind Key Done!") end
+        local nname
+        for i in pairs(Nm) do
+            nname='~!Num'..i
+            DeleteMacro(nname)
+            DeleteMacro(nname)
+            CreateMacro(nname,460699,Nm[i] ,true)
+end end end
+SetKey(true)

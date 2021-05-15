e2d0ea18dce29feb1df138b0786658408e327f2a
nuttin0011 <nuttin0011@hotmail.com>
2021-05-15 09:33:00 +0700

    Update ManyFunctionSnippet_Mage.lua

--- modules/ManyFunctionSnippet_Mage.lua
+++ modules/ManyFunctionSnippet_Mage.lua
@@ -27,30 +27,40 @@ IROVar.Mage.CastSequenceCheck={}
 
 function IROVar.Mage.CombatEvent()
     local _,subevent,_,sourceGUID,_,_,_,_,_,_,_,_,spellName=CombatLogGetCurrentEventInfo()
-    if (subevent~="SPELL_CAST_SUCCESS") or (sourceGUID~=IROVar.Mage.playerGUID) then return end
-    local function destroySequence(se,finishSequence)
-        se.canceled=true
-        if finishSequence or se.run_callback_when_timeout then
-            se.callBack()
-        end
-    end
-    for k,v in pairs(IROVar.Mage.CastSequenceCheck) do
-        if GetTime()>v.timeOut then
-            destroySequence(v,false)
-        end
-        if v.spellName[v.checkOrder]==spellName then
-            if (v.checkOrder==1)and v.timeout_after1stSpell then
-                C_Timer.After(v.timeout_after1stSpell,IROVar.Mage.checkSequenceTimeOut)
-                v.timeOut=GetTime()+v.timeout_after1stSpell
-            end
-            v.checkOrder=v.checkOrder+1
-            if v.spellName[v.checkOrder]==nil then
-                destroySequence(v,true)
+    if (sourceGUID~=IROVar.Mage.playerGUID) then return end
+    if (subevent=="SPELL_CAST_SUCCESS")then
+        local function destroySequence(se,finishSequence)
+            se.canceled=true
+            if finishSequence or se.run_callback_when_timeout then
+                se.callBack()
             end
         end
-        if IROVar.Mage.CastSequenceCheck[k].canceled then
-            IROVar.Mage.CastSequenceCheck[k]=nil
+        for k,v in pairs(IROVar.Mage.CastSequenceCheck) do
+            if GetTime()>v.timeOut then
+                destroySequence(v,false)
+            end
+            if v.spellName[v.checkOrder]==spellName then
+                if (v.checkOrder==1)and v.timeout_after1stSpell then
+                    C_Timer.After(v.timeout_after1stSpell,IROVar.Mage.checkSequenceTimeOut)
+                    v.timeOut=GetTime()+v.timeout_after1stSpell
+                end
+                v.checkOrder=v.checkOrder+1
+                if v.spellName[v.checkOrder]==nil then
+                    destroySequence(v,true)
+                end
+            end
+            if IROVar.Mage.CastSequenceCheck[k].canceled then
+                IROVar.Mage.CastSequenceCheck[k]=nil
+            end
         end
+    elseif (subevent=="SPELL_AURA_APPLIED") and (spellName=="Brain Freeze") then
+        print("Brain freezeeeeee")
+        IROVar.Mage.BrainFreezeStatus=IROVar.Mage.BrainFreezeStatus+1
+        local flurryFunc=function()
+            IROVar.Mage.BrainFreezeStatus=IROVar.Mage.BrainFreezeStatus-1
+            IROVar.Mage.currentFlurry=(IROVar.Mage.currentFlurry==1) and 2 or 1
+        end
+        IROVar.Mage.registerCheckSpellSequence("Flurry|Ice Lance|Ice Lance",12,5,flurryFunc,true)
     end
 end
 IROVar.Mage.frame =CreateFrame("Frame")
@@ -86,4 +96,13 @@ function IROVar.Mage.registerCheckSpellSequence(sequence,timeout,timeout_after1s
         canceled = false,
     })
     C_Timer.After(timeout,IROVar.Mage.checkSequenceTimeOut)
+end
+IROVar.Mage.currentFlurry=1
+IROVar.Mage.BrainFreezeStatus=0
+function IROVar.Mage.UseFlurry(n)
+    -- seperate flurry IL combo to 2 macro
+    -- 1 /cast reset=1.5 flurry, Ice Lance, Ice Lance
+    -- 2 /cast reset=1.6 flurry, Ice Lance, Ice Lance
+    -- this function return true/false of macro n use?
+    return (IROVar.Mage.currentFlurry==n) and (IROVar.Mage.BrainFreezeStatus>=1)
 end

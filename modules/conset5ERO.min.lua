d83321fa6942acb6da462034d05b2273234d7809
nuttin0011 <nuttin0011@hotmail.com>
2021-08-30 23:23:17 +0700

    Update conset5ERO.min.lua

--- modules/conset5ERO.min.lua
+++ modules/conset5ERO.min.lua
@@ -1,2 +1,228 @@
--- ERO DPS Decoder 9.0.5/6b
-if not IROUsedSkillControl then IROUsedSkillControl={}end;IROUsedSkillControl.IdleTimeAfterUseSkill=0.2;IROUsedSkillControl.PlayerSpec=GetSpecializationInfo(GetSpecialization())IROUsedSkillControl.AdjustIdleTimeAfterUseSkill=function()IROUsedSkillControl.PlayerSpec=GetSpecializationInfo(GetSpecialization())local a=IROUsedSkillControl.ClassType[IROUsedSkillControl.PlayerSpec][6]if a=='Caster'then IROUsedSkillControl.IdleTimeAfterUseSkill=0.4 else IROUsedSkillControl.IdleTimeAfterUseSkill=0.4 end end;C_Timer.After(5,IROUsedSkillControl.AdjustIdleTimeAfterUseSkill)IROUsedSkillControl.f2=CreateFrame("Frame")IROUsedSkillControl.f2:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")IROUsedSkillControl.f2:SetScript("OnEvent",IROUsedSkillControl.AdjustIdleTimeAfterUseSkill)IROUsedSkillControl.pingadjust=0.25;IROUsedSkillControl.ClassType={[71]={'B','Pummel','Pummel','Melee','DPS','InstanceCast'},[72]={'B','Pummel','Pummel','Melee','DPS','InstanceCast'},[73]={'A','Pummel','Pummel','Melee','Tank','InstanceCast'},[265]={'D','Command Demon','Corruption','Range','DPS','Caster'},[266]={'D','Command Demon','Corruption','Range','DPS','Caster'},[267]={'D','Command Demon','Corruption','Range','DPS','Caster'},[262]={'C','Wind Shear','Lightning Bolt','Range','DPS','Caster'},[263]={'B','Wind Shear','primal strike','Melee','DPS','InstanceCast'},[264]={'D','Wind Shear','Lightning Bolt','Range','Healer','Caster'},[259]={'B','Kick','Kick','Melee','DPS','InstanceCast'},[260]={'B','Kick','Kick','Melee','DPS','InstanceCast'},[261]={'B','Kick','Kick','Melee','DPS','InstanceCast'},[256]={'N','','Smite','Range','Healer','Caster'},[257]={'N','','Smite','Range','Healer','Caster'},[258]={'D','Silence','Smite','Range','DPS','Caster'},[65]={'N','','Crusader Strike','Range','Healer','Caster'},[66]={'A','Rebuke','Crusader Strike','Melee','Tank','InstanceCast'},[70]={'B','Rebuke','Crusader Strike','Melee','DPS','InstanceCast'},[268]={'A','Spear Hand Strike','Tiger Palm','Melee','Tank','InstanceCast'},[270]={'N','','Tiger Palm','Range','Healer','Caster'},[269]={'B','Spear Hand Strike','Tiger Palm','Melee','DPS','InstanceCast'},[62]={'C','Counterspell','Fire Blast','Range','DPS','Caster'},[63]={'C','Counterspell','Fire Blast','Range','DPS','Caster'},[64]={'C','Counterspell','Fire Blast','Range','DPS','Caster'},[253]={'C','Counter Shot','Arcane Shot','Range','DPS','InstanceCast'},[254]={'C','Counter Shot','Arcane Shot','Range','DPS','InstanceCast'},[255]={'C','Muzzle','Raptor Strike','Melee','DPS','InstanceCast'},[102]={'C','Solar Beam','Moonfire','Range','DPS','Caster'},[103]={'B','Skull Bash','Rake','Melee','DPS','InstanceCast'},[104]={'A','Skull Bash','Mangle','Melee','Tank','Caster'},[105]={'N','','Moonfire','Range','Healer','Caster'},[577]={'B','Disrupt','Chaos Strike','Melee','DPS','InstanceCast'},[581]={'A','Disrupt','Chaos Strike','Melee','Tank','InstanceCast'},[250]={'A','Mind Freeze','Death Strike','Melee','Tank','InstanceCast'},[251]={'B','Mind Freeze','Death Strike','Melee','DPS','InstanceCast'},[252]={'B','Mind Freeze','Death Strike','Melee','DPS','InstanceCast'}}local b=TMW.GCDSpell;local GetSpellCooldown=GetSpellCooldown;local UnitCastingInfo=UnitCastingInfo;local UnitChannelInfo=UnitChannelInfo;local GetTime=GetTime;local c={["00"]=0x00,["21"]=0x01,["22"]=0x02,["23"]=0x03,["24"]=0x04,["25"]=0x05,["26"]=0x06,["27"]=0x07,["28"]=0x08,["29"]=0x09,["2a"]=0x0a,["2b"]=0x0b,["2c"]=0x0c,["30"]=0x0d,["31"]=0x0e,["32"]=0x0f,["33"]=0x10,["34"]=0x11,["35"]=0x12,["36"]=0x13,["37"]=0x14,["38"]=0x15,["39"]=0x16,["3a"]=0x17,["3b"]=0x18,["3c"]=0x19,["3d"]=0x1a,["3e"]=0x1b,["55"]=0x1c}local d={["00"]=0,["01"]=bit.lshift(0x01,5),["02"]=bit.lshift(0x02,5),["03"]=bit.lshift(0x03,5),["04"]=bit.lshift(0x04,5),["05"]=bit.lshift(0x05,5),["06"]=bit.lshift(0x06,5),["07"]=bit.lshift(0x07,5)}function enSubMiniIROCode(e)if not e then return"00"end;local f=d[string.sub(e,3,4)]if not f then return"00"end;local g=c[string.sub(e,5,6)]if not g then return"00"end;return string.format("%02x",g+f)end;function getMetaIconColor(h)local i=h.__currentIcon;local j=i and i.__currentIcon or nil;while j do i=j;j=i.__currentIcon end;local k=i and i.States[1].Color or"ff000000"return k end;function enMiniIROcode(l,m,n)local o=enSubMiniIROCode(l)local p=enSubMiniIROCode(m)local q=enSubMiniIROCode(n)return"ff"..q..p..o end;local r={}r.now=0;r.adjustTiming=5.14;r.adjustWorldPing=function()r.now=select(4,GetNetStats())/1000;C_Timer.After(r.adjustTiming,r.adjustWorldPing)end;r.adjustWorldPing()local s=0.2;function NextTimeCheckLockUseSkill(t)local u,v=GetSpellCooldown(b)local w,x,x,x,y=UnitCastingInfo("player")if not w then w,x,x,x,y=UnitChannelInfo("player")end;local z=GetTime()local A,B;t=t or s;if w then A=y/1000;B=A-(r.now+t)if z<B then return B,false else return A+t,true end else if not u then return z+0.3,true else A=u+v;B=A-(r.now+t)if z<B then return B,false else return A+t,true end end end end;IROUsedSkillControl.Stage=1;IROUsedSkillControl.SkillHandle={}IROUsedSkillControl.PlayerGUID=UnitGUID("player")IROUsedSkillControl.NextForceReady=0;IROUsedSkillControl.OldTimeNumDotPress=0;function IROUsedSkillControl.OnEvent()local x,C,x,D=CombatLogGetCurrentEventInfo()if D==IROUsedSkillControl.PlayerGUID and C=="SPELL_CAST_FAILED"then IROUsedSkillControl.forceReady()end end;IROUsedSkillControl.f=CreateFrame("Frame")IROUsedSkillControl.f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")IROUsedSkillControl.f:SetScript("OnEvent",IROUsedSkillControl.OnEvent)IROUsedSkillControl.KeepLogOffGCD=function()if IROUsedSkillControl.KeepLogText then IROUsedSkillControl.KeepLogText(true)end end;IROUsedSkillControl.NumDotPress=function()if IROUsedSkillControl.KeepLogText then IROUsedSkillControl.KeepLogText()end;IROUsedSkillControl.Stage=2;IROUsedSkillControl.Stage2to4()end;IROUsedSkillControl.NotReadyToUseSkill=function()return IROUsedSkillControl.Stage~=1 end;IROUsedSkillControl.forceReady=function()local z=GetTime()if IROUsedSkillControl.NextForceReady>z then return end;IROUsedSkillControl.NextForceReady=z+0.19;for x,E in pairs(IROUsedSkillControl.SkillHandle)do E:Cancel()end;IROUsedSkillControl.SkillHandle={}IROUsedSkillControl.Stage=1 end;IROUsedSkillControl.Stage2to4=function()IROUsedSkillControl.Stage=3;table.insert(IROUsedSkillControl.SkillHandle,C_Timer.NewTimer(IROUsedSkillControl.IdleTimeAfterUseSkill,IROUsedSkillControl.CheckStageto4))end;IROUsedSkillControl.CheckStageto4=function()if IROUsedSkillControl.Stage~=1 then IROUsedSkillControl.Stage=4;IROUsedSkillControl.Stage4to1()end end;IROUsedSkillControl.Stage4to1=function()local z;local F,G=NextTimeCheckLockUseSkill(IROUsedSkillControl.pingadjust)if G then IROUsedSkillControl.forceReady()else IROUsedSkillControl.Stage=5;z=GetTime()local H=F-z;if H>0.2 then H=0.2 end;if H<0 then H=0 end;table.insert(IROUsedSkillControl.SkillHandle,C_Timer.NewTimer(H,IROUsedSkillControl.CheckStageto4))end end
+
+
+
+-- ERO DPS Decoder 9.0.5/7
+
+-- can copy this to LUA Snippted
+-- Set to Hiest Priority!!!!!!!!!!!!!!
+-- Setup UsedSkill System
+
+--Wait For Use Skill Icon Control
+--can use skill when "IROUsedSkillControl.Stage=1"
+--show icon that block Rotation "IROUsedSkillControl.NotReadyToUseSkill()==true"
+--@Numdot
+--/run IROUsedSkillControl.NumDotPress() <-- keep log for in GCD skill
+--/run IROUsedSkillControl.KeepLogOffGCD() <-- keep log off GCD skill
+--@etc
+--/run IROUsedSkillControl.forceReady() <-- forceReady
+--log work only with EROTools Addon installed
+
+if not IROUsedSkillControl then
+	IROUsedSkillControl={}
+end
+IROUsedSkillControl.KeepLogOffGCD = function()
+	if IROUsedSkillControl.KeepLogText then IROUsedSkillControl.KeepLogText(true) end
+end
+IROUsedSkillControl.Stage=1
+IROUsedSkillControl.GCDCD=1
+IROUsedSkillControl.PlayerSpec=GetSpecializationInfo(GetSpecialization())
+IROUsedSkillControl.spec1secGCD = {
+	[259] = true-- Ass
+	,[260] = true -- Out
+	,[261] = true -- Sub
+	,[103] = true -- Feral
+	,[269] = true -- Windwalker
+}
+
+function IROUsedSkillControl.Haste_Event(Self,Event,Arg1)
+	if(Arg1=="player")and(not IROUsedSkillControl.spec1secGCD[IROUsedSkillControl.PlayerSpec])then
+        IROUsedSkillControl.GCDCD = math.max(0.5,1.5*(100/(100+UnitSpellHaste("player"))))
+	end
+end
+IROUsedSkillControl.Haste_Event(nil,nil,"player")
+IROUsedSkillControl.f1=CreateFrame("Frame")
+IROUsedSkillControl.f1:RegisterEvent("UNIT_SPELL_HASTE")
+IROUsedSkillControl.f1:SetScript("OnEvent", IROUsedSkillControl.Haste_Event)
+
+function IROUsedSkillControl.SpecChanged()
+	local spec=GetSpecializationInfo(GetSpecialization())
+	if IROUsedSkillControl.spec1secGCD[spec] then
+		IROUsedSkillControl.GCDCD=1
+	else
+		IROUsedSkillControl.GCDCD=math.max(0.5,1.5*(100/(100+UnitSpellHaste("player"))))
+	end
+	IROUsedSkillControl.PlayerSpec=spec
+end
+C_Timer.After(5,IROUsedSkillControl.SpecChanged)
+IROUsedSkillControl.f2 = CreateFrame("Frame")
+IROUsedSkillControl.f2:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+IROUsedSkillControl.f2:SetScript("OnEvent", IROUsedSkillControl.SpecChanged)
+
+function IROUsedSkillControl.Cast_OnEvent(self,Event,Unit,CastID,SpellID)
+	if (Unit ~= "player") then return end -- if not player return
+    if Event == "UNIT_SPELLCAST_START" then
+        IROUsedSkillControl.StopGCDPluse()
+        IROUsedSkillControl.CreateCastPluse()
+    elseif Event == "UNIT_SPELLCAST_STOP" then
+       -- if not IROUsedSkillControl.GCDPluseActive
+        -- ไปดูก่อนว่า จะ Start ก่อน Stop ได้ไหม ? ถ้าไม่ได้ให้ทำ
+        IROUsedSkillControl.SpellActive=false
+        if IROUsedSkillControl.GCDPluseTimeStamp<=IROUsedSkillControl.SpellTimeStamp then
+            IROUsedSkillControl.forceReady()
+        end
+    end
+end
+
+IROUsedSkillControl.f3 = CreateFrame("Frame")
+IROUsedSkillControl.f3:RegisterEvent("UNIT_SPELLCAST_START")
+IROUsedSkillControl.f3:RegisterEvent("UNIT_SPELLCAST_STOP")
+IROUsedSkillControl.f3:SetScript("OnEvent", IROUsedSkillControl.Cast_OnEvent)
+
+IROUsedSkillControl.GCDTickHandle=C_Timer.NewTimer(0,function() end)
+IROUsedSkillControl.GCDPluseActive=false
+IROUsedSkillControl.GCDPluseTimeStamp=0
+IROUsedSkillControl.SpellActive=false
+IROUsedSkillControl.SpellTimeStamp=0
+IROUsedSkillControl.forceReadyCheckHandle=C_Timer.NewTimer(0,function() end)
+
+function IROUsedSkillControl.forceReadyCheck()
+    if IROUsedSkillControl.Stage==1 then
+        IROUsedSkillControl.GCDTickHandle:Cancel()
+    end
+end
+
+function IROUsedSkillControl.forceReady()
+    IROUsedSkillControl.forceReadyCheckHandle:Cancel()
+    IROUsedSkillControl.Stage=1
+    IROUsedSkillControl.forceReadyCheckHandle=C_Timer.NewTimer(0.4,IROUsedSkillControl.forceReadyCheck)
+end
+
+function IROUsedSkillControl.RepeatGCDPluse()
+    IROUsedSkillControl.forceReady()
+    IROUsedSkillControl.GCDTickHandle=C_Timer.NewTimer(IROUsedSkillControl.GCDCD,IROUsedSkillControl.RepeatGCDPluse)
+end
+
+function IROUsedSkillControl.CreateNewGCDPluse()
+    IROUsedSkillControl.GCDTickHandle:Cancel()
+    IROUsedSkillControl.GCDPluseActive=true
+    IROUsedSkillControl.GCDPluseTimeStamp=GetTime()
+    IROUsedSkillControl.GCDTickHandle=C_Timer.NewTimer(IROUsedSkillControl.GCDCD-0.05,
+    function()
+        IROUsedSkillControl.RepeatGCDPluse()
+    end)
+end
+
+function IROUsedSkillControl.StopGCDPluse()
+    IROUsedSkillControl.GCDTickHandle:Cancel()
+    IROUsedSkillControl.GCDPluseActive=false
+end
+
+function IROUsedSkillControl.CreateCastPluse()
+    IROUsedSkillControl.SpellActive=true
+    IROUsedSkillControl.SpellTimeStamp=GetTime()
+    local n, _, _, _, endTimeMS= UnitCastingInfo("player")
+    if not n then
+        n, _, _, _, endTimeMS= UnitChannelInfo("player")
+    end
+    endTimeMS=(endTimeMS/1000)-GetTime()-0.4
+    IROUsedSkillControl.GCDTickHandle=C_Timer.NewTimer(endTimeMS,
+    function()
+        IROUsedSkillControl.forceReady()
+    end)
+end
+
+function IROUsedSkillControl.NumDotPress()
+    if IROUsedSkillControl.KeepLogText then IROUsedSkillControl.KeepLogText() end
+    IROUsedSkillControl.Stage=2
+    if not IROUsedSkillControl.GCDPluseActive then
+        IROUsedSkillControl.CreateNewGCDPluse()
+    end
+end
+
+-- this function Change Normal IROcode --> miniIROCode
+-- it's ll compress 3 Icon --> 1 Icon
+-- ctrl		= 001 00000 = 32	= 0x20
+-- alt		= 010 00000 = 64	= 0x40
+-- shift	= 100 00000 = 128	= 0x80
+-- e.g.
+-- c1 = "ff002100" (F1)			--> 0x01
+-- c2 = "ff003000" ({Numpad0})	--> 0x0d
+-- c3 = "ff042604" (Shift-F6)	--> 0x06 + 0x80 (shift code) --> 0x86
+-- c1+c2+c3 = miniIROCode = "ff860d01"
+-- NOTE. need to swap byte c1 and c3 cause of EroDPS PixelGetColor function
+-- enMiniIROcode("ff002100","ff003000","ff042604")="ff860d01"
+
+local IROcolorCode ={
+	["00"]=0x00,
+	["21"]=0x01, --{F1}
+	["22"]=0x02, --{F2}
+	["23"]=0x03, --{F3}
+	["24"]=0x04, --{F4}
+	["25"]=0x05, --{F5}
+	["26"]=0x06, --{F6}
+	["27"]=0x07, --{F7}
+	["28"]=0x08, --{F8}
+	["29"]=0x09, --{F9}
+	["2a"]=0x0a, --{F10}
+	["2b"]=0x0b, --{F11}
+	["2c"]=0x0c, --{F12}
+	["30"]=0x0d, --{Numpad0}
+	["31"]=0x0e, --{Numpad1}
+	["32"]=0x0f, --{Numpad2}
+	["33"]=0x10, --{Numpad3}
+	["34"]=0x11, --{Numpad4}
+	["35"]=0x12, --{Numpad5}
+	["36"]=0x13, --{Numpad6}
+	["37"]=0x14, --{Numpad7}
+	["38"]=0x15, --{Numpad8}
+	["39"]=0x16, --{Numpad9}
+	["3a"]=0x17, --{NumpadDot}
+	["3b"]=0x18, --{NumpadAdd}
+	["3c"]=0x19, --{NumpadSub}
+	["3d"]=0x1a, --{NumpadMult}
+	["3e"]=0x1b, --{NumpadDiv}
+	["55"]=0x1c, --MoveJump ; 0x005500 : MoveJump
+}
+
+-- 1 = ctrl , 2 = alt , 4 = shift
+-- 1 = 001 , 2 = 010 , 4 = 100
+local modStr ={
+	["00"]=0,
+	["01"]=bit.lshift(0x01,5),
+	["02"]=bit.lshift(0x02,5),
+	["03"]=bit.lshift(0x03,5),
+	["04"]=bit.lshift(0x04,5),
+	["05"]=bit.lshift(0x05,5),
+	["06"]=bit.lshift(0x06,5),
+	["07"]=bit.lshift(0x07,5),
+}
+
+function enSubMiniIROCode(IROcode)
+	--"ff042604" (Shift-F6)	--> 0x06 + 0x20 (shift code) --> 0x26
+	-- if error return "00"
+	if not IROcode then return "00" end
+	local modstr = modStr[string.sub(IROcode,3,4)]
+	if not modstr then return "00" end
+	local miniIROCode = IROcolorCode[string.sub(IROcode,5,6)]
+	if not miniIROCode then return "00" end
+	return string.format("%02x",miniIROCode+modstr)
+end
+
+function getMetaIconColor(icon)
+	if not icon then return "ff000000" end
+	local a=icon.__currentIcon
+	local b=a and a.__currentIcon or nil
+	while b do
+		a=b
+		b=a.__currentIcon
+	end
+	local c=a and a.States[1].Color or "ff000000"
+	return c
+end
+
+function enMiniIROcode(IROcode1,IROcode2,IROcode3)
+	local miniIROCode1 = enSubMiniIROCode(IROcode1)
+	local miniIROCode2 = enSubMiniIROCode(IROcode2)
+	local miniIROCode3 = enSubMiniIROCode(IROcode3)
+	return "ff"..miniIROCode3..miniIROCode2..miniIROCode1
+end

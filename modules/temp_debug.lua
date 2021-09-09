12bae9c032eefc68d2883273365c5ae95fd9bb0d
nuttin0011 <nuttin0011@hotmail.com>
2021-09-09 17:34:52 +0700

    Update temp_debug.lua

--- modules/temp_debug.lua
+++ modules/temp_debug.lua
@@ -1,38 +1,62 @@
--- Key Setting...
-local Nm={}
 
+local function checkUnit(n)
+    return UnitExists(n) and
+    UnitCanAttack("player",n) and
+    (UnitAffectingCombat(n) or IsItemInRange(32321,n))and --32321, -- Sparrowhawk Net 10 yard
+    (not UnitIsDead(n)) and
+    (IsSpellInRange("Mind Blast","target")==1)
+end
 
-Nm[1]=''
-Nm[2]=''
+local function selectUbyHP(u1,u2)
+    return (u1 and (UnitHealth(u1)<=UnitHealth(u2))) and u1 or u2
+end
 
+local u=nil
+--target
+if checkUnit("target") then
+    u="target"
+end
+--mouseover
+if checkUnit("mouseover") then
+    u=selectUbyHP(u,"mouseover")
+end
+--focus
+if checkUnit("focus") then
+    u=selectUbyHP(u,"focus")
+end
+local uI
+--partyXtarget
+for i=1,4 do
+    if not UnitExists("party"..i) then break end
+    uI="party"..i.."target"
+    if checkUnit(uI) then
+        u=selectUbyHP(u,uI)
+    end
+end
+--raidXtarget
+for i=1,13 do
+    if not UnitExists("raid"..i) then break end
+    uI="raid"..i.."target"
+    if checkUnit(uI) then
+        u=selectUbyHP(u,uI)
+    end
+end
 
-
-
---print((IROVar.InterruptSpell~=nil) and IROVar.InterruptSpell or "nil")
---print((IROSpecID~=nil) and IROSpecID or "nil")
-local iS=IROVar.InterruptSpell or ""
-Nm[8]='/use [mod:ctrl,nomod:alt]13\n/use [mod:ctrl,nomod:alt]14\n/targetenemy [nomod:ctrl,mod:alt]\n/cleartarget [nomod]'
-Nm[9]='/focus [@mouseover,exists,harm,nodead,mod:ctrlalt]'..
-'\n/cast [mod:ctrl,nomod:alt,@focus]'..iS..';[mod:alt,nomod:ctrl]'..iS..
-'\n/stopmacro [mod]'..
-'\n/run IROUsedSkillControl.KeepLogOffGCD()'
-local function SetKey(IncombatStatus)
-    if InCombatLockdown() then
-        if IncombatStatus then print("cannot Bind Key while Incombat") end
-        C_Timer.After(1,SetKey)
+local cc=IROVar.Healer.UnitToIRO[u or 1]
+if cc and EROSWDIcon1.States[1].Color~=cc then
+    if EROSWDIcon1 then
+        EROSWDIcon1.States[1].Color=cc
+    end
+    if EROSWDIcon2 then
+        EROSWDIcon2.States[1].Color=cc
+    end
+    if TMW_ST:GetCounter("swdone")==0
+    then
+        TMW_ST:UpdateCounter("swdone",1)
     else
-        if not IncombatStatus then print("Out Combat Bind Key Done!") end
-        local nname
-        for i in pairs(Nm) do
-            nname='~!Num'..i
-            DeleteMacro(nname)
-            DeleteMacro(nname)
-            if select(2,GetNumMacros()) >=18 then
-                print('ERROR------------"')
-                print('Cannot Create More Macro, Please Delete More "Specific Macro"')
-                print('and use "/reload"')
-            end
-            CreateMacro(nname,460699,Nm[i] ,true)
-end end end
-SetKey(true)
+        TMW_ST:UpdateCounter("swdone",0)
+    end
+elseif cc==nil then
+    TMW_ST:UpdateCounter("swdone",3)
+end
 

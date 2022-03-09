f16bd5d1dc281b46f350d696572df328e6ded841
nuttin0011 <nuttin0011@hotmail.com>
2022-03-10 00:27:22 +0700

    Create DPSRotation_Code_Style.lua

---
+++ modules/DPSRotation_Code_Style.lua
@@ -0,0 +1,9 @@
+--DPSRotation_Code_Style Base Template v1
+--*****Set Priority to 2
+--*****Set DPSRotation of Class to 3
+
+EROCast={}
+EROCast.Spell1=nil --On GCD
+EROCast.Spell2=nil --Off GCD
+EROCast.Spell3=nil --Off GCD
+

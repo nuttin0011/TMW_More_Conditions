f2f20fbcbbf7508bd287ce33ad9b9ecb7f7ff6ed
nuttin0011 <nuttin0011@hotmail.com>
2022-03-10 00:27:24 +0700

    Create DPSRotation_Code_WarFury.lua

---
+++ modules/DPSRotation_Code_WarFury.lua
@@ -0,0 +1,9 @@
+--DPSRotation_Code_WarFury v1
+--*****Set DPSRotation of Class to 3
+
+-- RP = Rampage
+-- BT = Bloodthirst
+-- RB = Raging Blow
+-- WW = Whirlwind
+-- EX = Execute
+-- EXm = Execute @ mouse over

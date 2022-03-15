0c553d45891ed2e7d00b13e3ee390e1e9eee71b3
nuttin0011 <nuttin0011@hotmail.com>
2022-03-15 14:24:11 +0700

    Update DPSRotation_Code_Style.lua

--- modules/DPSRotation_Code_Style.lua
+++ modules/DPSRotation_Code_Style.lua
@@ -2,8 +2,16 @@
 --*****Set Priority to 2
 --*****Set DPSRotation of Class to 3
 
-EROCast={}
-EROCast.Spell1=nil --On GCD
-EROCast.Spell2=nil --Off GCD
-EROCast.Spell3=nil --Off GCD
+IROVar.CastIcon1 = "ff000000"
+IROVar.CastIcon2 = "ff000000"
+IROVar.CastIcon3 = "ff000000"
 
+function IROVar.Cast1(s)
+    IROVar.CastIcon1 = IUSC.SpellToColor and IUSC.SpellToColor[s] or "ff000000"
+end
+function IROVar.Cast2(s)
+    IROVar.CastIcon2 = IUSC.SpellToColor and IUSC.SpellToColor[s] or "ff000000"
+end
+function IROVar.Cast3(s)
+    IROVar.CastIcon3 = IUSC.SpellToColor and IUSC.SpellToColor[s] or "ff000000"
+end

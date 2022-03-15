781bdeb4c9ff14cd62b14af62e40452142d4e5ee
nuttin0011 <nuttin0011@hotmail.com>
2022-03-15 20:35:20 +0700

    Update CareBurn.lua

--- modules/CareBurn.lua
+++ modules/CareBurn.lua
@@ -1,4 +1,4 @@
--- Care Burn 1.7 Icon
+-- Care Burn 1.7b Icon
 -- Burst Only Condition met / no condition on this mob
 -- IROVar.CareBurn(spec) ; return true / false ; can use only "target"
 
@@ -36,31 +36,16 @@ if not IROVar.MobListForBurn then
         ["Spires of Ascension"] ={
             ["Oryphrion"]=[[return TMW.CNDT.Env.AuraDur("target", "drained", "HARMFUL")>10]],
         },
-        ["Castle Nathria"]={
-            --burn after impact
-            ["Sludgefist"]=[[return TMW.CNDT.Env.AuraDur("target", "destructive impact", "HELPFUL")>0]],
-            --burn 2 sec befor run impact , 253=Hunter BM ,72=war fury
-            ["Sludgefist253"]=[[local t=TMW.CNDT.Env.AuraDur("targettarget", "hateful gaze", "HARMFUL");return ((t>0)and(t<2))or(TMW.CNDT.Env.AuraDur("target", "destructive impact", "HELPFUL")>0)]],
-            ["Sludgefist72"]=[[local t=TMW.CNDT.Env.AuraDur("targettarget", "hateful gaze", "HARMFUL");return ((t>0)and(t<2))or(TMW.CNDT.Env.AuraDur("target", "destructive impact", "HELPFUL")>0)]],
-            --not has Shield
-            ["General Kaal"]=[[return TMW.CNDT.Env.AuraDur("target", "hardened stone form", "HELPFUL")==0]],
-            ["General Grashaal"]=[[return TMW.CNDT.Env.AuraDur("target", "hardened stone form", "HELPFUL")==0]],
-        },
+
     }
     IROVar.MobListForBurn.encounterID=nil
     IROVar.MobListForBurn.encounterStart=GetTime()
-    IROVar.MobListForBurn.enTimer1=0
-    IROVar.MobListForBurn.enTimer2=0
-    IROVar.MobListForBurn.enTimer3=0
     if not IROVar.MobListForBurn.event then
         IROVar.MobListForBurn.encounterStart=0
         IROVar.MobListForBurn.eventFunc=function(_,event,encounterID, encounterName, difficultyID, groupSize,success)
             if event=="ENCOUNTER_START" then
                 IROVar.MobListForBurn.encounterID=encounterID
                 IROVar.MobListForBurn.encounterStart=GetTime()
-                IROVar.MobListForBurn.enTimer1=0
-                IROVar.MobListForBurn.enTimer2=0
-                IROVar.MobListForBurn.enTimer3=0
             elseif event=="ENCOUNTER_END" then
                 IROVar.MobListForBurn.encounterID=nil
             end
@@ -108,20 +93,6 @@ if not IROVar.MobListForBurn then
     end
 end
 
-
-
-IROVar.MobListForBurnByEncounterID = {
-    [2383]={
-        timer_name ={"timer1","timer2","timer3"},
-        start_up = [[reset("timer1") reset("timer2")]],
-        register_event ={
-            ["SPELL_CAST_START 22354"]=[[reset("timer1") reset("timer2")]],
-            ["SPELL_AURA_APPLIED 55445"]=[[reset("timer3")]]
-        },
-        condition=[[return (TMW.CNDT.Env.AuraDur("player","debuff", "HARMFUL")==0)and (val("timer1")<25)]],
-        condition72=[[return true]],
-    }
-}
 
 
 

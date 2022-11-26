382446dabf857eeb4be7808f99ecf361476bd6eb
nuttin0011 <nuttin0011@hotmail.com>
2022-11-26 18:56:14 +0700

    Update ManyFunctionSnippet_AuraTracker.lua

--- modules/ManyFunctionSnippet_AuraTracker.lua
+++ modules/ManyFunctionSnippet_AuraTracker.lua
@@ -1,14 +1,19 @@
--- Many Function Aura Tracker 10.0.0/5
+-- Many Function Aura Tracker 10.0.0/6
 -- Set Priority to 5
 
 --function IROVar.Aura.Register_UNIT_AURA_scrip_CALLBACK(name,callback)
 --function IROVar.Aura.UnRegister_UNIT_AURA_scrip_CALLBACK(name)
 
---Aura1 , Track only Name/ID/ExpTime ( if multiple aura use shortest ExpTime)
+--Aura1 , Track Player Name/ID/ExpTime ( if multiple aura use shortest ExpTime)
 --function IROVar.Aura1.RegisterTrackedAura(list)
 --function IROVar.Aura1.UnRegisterTrackedAura(list)
 --function IROVar.Aura1.GetAura(n) -- return ExpTime , nil = no aura , 0 = no ExpTime
     -- or use IROVar.Aura1.My[n]
+
+-- Aura2 At Target keep only Name , id
+--function IROVar.Aura2.RegisterTrackedAura(aura,filter)
+--function IROVar.Aura2.UnRegisterTrackedAura(aura,filter)
+--function IROVar.Aura2.GetAura(auraName,filter)
 
 if not IROVar then IROVar = {} end
 if not IROVar.Aura then IROVar.Aura = {} end
@@ -87,7 +92,8 @@ function IROVar.Aura1.DumpAura()
         if IROVar.Aura1.TrackedAura[n] then
             IROVar.Aura1.My[n]=IROVar.Aura1.My[n] and math.min(e,IROVar.Aura1.My[n]) or e
             IROVar.Aura1.Changed[n]=true
-        elseif IROVar.Aura1.TrackedAura[s] then
+        end
+        if IROVar.Aura1.TrackedAura[s] then
             IROVar.Aura1.My[s]=IROVar.Aura1.My[s] and math.min(e,IROVar.Aura1.My[s]) or e
             IROVar.Aura1.Changed[s]=true
         end
@@ -107,7 +113,7 @@ function IROVar.Aura1.DumpAura()
         addAuraToTable(name,exp,spellId)
     end
     for k,v in pairs(OldAura) do
-        IROVar.Aura1.Changed[k]=not(v==IROVar.Aura1.My[k])
+        IROVar.Aura1.Changed[k]=v~=IROVar.Aura1.My[k]
     end
     --print("Dump At :",GetTime())
 end
@@ -119,9 +125,75 @@ C_Timer.After(2,function() -- Init Dump Aura
 end)
 
 -- Aura2 At Target keep only Name , id
---[[
+
 if not IROVar.Aura2 then IROVar.Aura2 = {} end
-IROVar.Aura2.My={}
+IROVar.Aura2.tar={}
 IROVar.Aura2.TrackedAura={}
 IROVar.Aura2.Changed={}
+
+--[[    IROVar.Aura2.TrackedAura={
+        ["filter"]={
+            ["aura Name"]=ExpTime,
+            [auraID]=ExpTime,
+        }
+    }
 ]]
+
+local function RegisterTracked2(aura,table,filter)
+    if not table[filter] then table[filter]={} end
+    table[filter][aura]=true
+end
+local function UnRegisterTracked2(aura,table,filter)
+    table[filter][aura]=nil
+    if next(table[filter])==nil then table[filter]=nil end
+end
+
+function IROVar.Aura2.RegisterTrackedAura(aura,filter)
+    RegisterTracked2(aura,IROVar.Aura2.TrackedAura,filter)
+end
+
+function IROVar.Aura2.UnRegisterTrackedAura(aura,filter)
+    UnRegisterTracked2(aura,IROVar.Aura2.TrackedAura,filter)
+end
+
+function IROVar.Aura2.DumpAura()
+    local OldAura=IROVar.Aura2.tar
+    IROVar.Aura2.Changed={}
+    IROVar.Aura2.tar={}
+    local name,exp,spellId,i
+    for filter,aur in pairs(IROVar.Aura2.TrackedAura) do
+        IROVar.Aura2.tar[filter]={}
+        IROVar.Aura2.Changed[filter]={}
+        i=1
+        name,_,_,_,_,exp,_,_,_,spellId=UnitAura("target",i,filter)
+        while name do
+            if aur[name]and(not IROVar.Aura2.tar[filter][name] or IROVar.Aura2.tar[filter][name]>exp)then
+                IROVar.Aura2.tar[filter][name]=exp
+                IROVar.Aura2.Changed[filter][name]=true
+            end
+            if aur[spellId]and(not IROVar.Aura2.tar[filter][spellId] or IROVar.Aura2.tar[filter][spellId]>exp) then
+                IROVar.Aura2.tar[filter][spellId]=exp
+                IROVar.Aura2.Changed[filter][spellId]=true
+            end
+            i=i+1
+            name,_,_,_,_,exp,_,_,_,spellId=UnitAura("target",i,filter)
+        end
+    end
+    for filter,SetAur in pairs(OldAura) do
+        for aur,ex in pairs(SetAur) do
+            IROVar.Aura2.Changed[filter][aur]=ex~=IROVar.Aura2.tar[filter][aur]
+        end
+    end
+end
+
+function IROVar.Aura2.GetAura(n,f)
+    return IROVar.Aura2.tar[f][n]
+end
+
+IROVar.Aura.Register_UNIT_AURA_scrip_CALLBACK("IROVar.Aura2.tar",function(unit)
+    if unit=="target" then IROVar.Aura2.DumpAura()end
+end)
+IROVar.Register_PLAYER_TARGET_CHANGED_scrip_CALLBACK("IROVar.Aura2.tar",IROVar.Aura2.DumpAura)
+C_Timer.After(2.1,function() -- Init Dump Aura
+    for _,v in ipairs(IROVar.Aura.CallBack) do v[2]("target")end
+end)
